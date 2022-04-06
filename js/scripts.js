9aa456b09674f48c11182187132ee5cb0a52d784
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-04-05 20:31:30 -0700

    add info on animals and scripts to show and hide
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>
    dquote> co-authored-by: Adrienne Matosich <amatosich@gmail.com>
    '

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
-  $("form#animals").submit(function(event) {
+  $("form#animal").submit(function(event) {
     event.preventDefault();
     const animals = $("#animals").val();
+    if (animals === "turtles") {
+      $("#turtles").show();
+      $("#snakes").hide();
+      $("#alligators").hide();
+    } else if (animals === "snakes") {
+      $("#snakes").show();
+      $("#turtles").hide();
+      $("#alligators").hide();
+    } else if (animals === "alligators") {
+      $("#alligators").show();
+      $("#turtles").hide();
+      $("#snakes").hide();
+    }
+    
   });
 });
+
+
 
 
 
